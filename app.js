2dcade599170dea13694451c8c286b7a2e7e673f
AhmedBaba95 <ahmedbabamohamed95@gmail.com>
2023-10-23 13:41:00 +0000

    required pointInPolygon & states.json in app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const pointInPolygon = require('point-in-polygon');
+const states = require('./data/states.json');
 let args = process.argv.slice(2);
+
 const longitude = args[0];
 const latitude = args[1];
+
 console.log(`Point entered is: (${longitude}, ${latitude})`);
+
