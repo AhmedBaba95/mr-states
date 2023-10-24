806c0c8ae35389a3f0eb762142aa6cce8f3ca829
AhmedBaba95 <ahmedbabamohamed95@gmail.com>
2023-10-24 10:06:08 +0000

    handling error case: point doesn't belong to any polygon.

--- app.js
+++ app.js
@@ -8,28 +8,35 @@ const latitude = args[1];
 if(args.length == 0){
     console.log("Please Enter A Point") 
 }else if(args.length > 2) {
-     console.log("Point invalid")
+    console.log("Point invalid")
 } else {
-console.log(`Point entered is: (${longitude}, ${latitude})`);
+    console.log(`Point entered is: (${longitude}, ${latitude})`);
 
-let json_data = JSON.parse(JSON.stringify(states))
-let states_values = Object.values(json_data);
-let states_keys = Object.keys(json_data);
+    let json_data = JSON.parse(JSON.stringify(states))
+    let states_values = Object.values(json_data);
+    let states_keys = Object.keys(json_data);
 
-states_values.forEach((statePolygon, index) => {
+    states_values.forEach((statePolygon, index) => {
     
-    console.log(`polygon of ${states_keys[index]} is:` )
-    console.log(statePolygon)
+        console.log(`polygon of ${states_keys[index]} is:` )
+        console.log(statePolygon)
 
-});
+    });
 
-states_values.forEach((statePolygon, index) => {
+    let hasAState = false
 
-    let point = [longitude,latitude]
-    if(pointInPolygon(point, statePolygon)) {
-        console.log(`The point entered is in ${states_keys[index]}`)
+    states_values.forEach((statePolygon, index) => {
+
+        let point = [longitude,latitude]
+        if(pointInPolygon(point, statePolygon)) {
+            console.log(`The point entered is in ${states_keys[index]}`)
+            hasAState = true
+        }
+
+    });
+
+    if(!hasAState) {
+        console.log("The Point is not in any polygon")
     }
-
-});
 }
 
